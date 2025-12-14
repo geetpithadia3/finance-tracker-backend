46e9a48c55a4a1a481273f1ee4ff5f7a1567e7c8
Geet Pithadia <gpithadia@gmail.com>
2025-12-14 14:47:42 -0500

    Fix script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 if [ "${DATABASE_PROFILE:-sqlite}" = "postgresql" ]; then
   echo "Running Migrations..."
-  ./venv/bin/python -m alembic upgrade head
+  python -m alembic upgrade head
 fi
 
 # Start App
 echo "Starting Uvicorn..."
-exec ./venv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
